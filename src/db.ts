30da98c44d7f4d1b126468bc8e4bfb9aaaeaed33
Abhishek Yadav <abhishek_yadav_work@outlook.com>
2025-09-12 21:54:23 +0530

    db connect

---
+++ src/db.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { Pool } from "pg";
+
+export const pool = new Pool({
+  host: process.env.PGHOST,
+  port: process.env.PGPORT ? Number(process.env.PGPORT) : 5432,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD ? String(process.env.PGPASSWORD) : "",
+  database: process.env.PGDATABASE,
+});
+console.log(
+  "PGPASSWORD:",
+  typeof process.env.PGPASSWORD,
+  process.env.PGPASSWORD
+);
